a0a89d98677dac10fc4ee9a946631af17872977d
huangjunhao <huang.jun.hao@foxmail.com>
2024-04-03 10:53:24 +0800

    35

--- miniprogram-1/pages/index/index.js
+++ miniprogram-1/pages/index/index.js
@@ -1,23 +1,11 @@
 // index.js
 Page({
 
-    parentHandler(){
-        console.log('父组件绑定的事件')
-    },
-    btnHandler(){
-        console.log('子组件触发的事件')
-    },
-
-    //事件处理函数需要写到Page()方法中才可以
-    handler(event){
-        // console.log("事件触发了~~~")
-        console.log(event)
-    },
-    getInputVal(event){
-        console.log(event.detail.value)
-    },
+   
     //在小程序页面中所需要使用的数据均来自data对象
     data:{
+        id:1,
+        isChecked: false,
         school: "尚硅谷",
         obj: {
             name: "tom"
